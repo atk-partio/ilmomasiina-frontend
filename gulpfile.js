4558de094634d1c18540bcca485adfdfcae82705
Jaakko Hynynen <jaakko.hynynen@gmail.com>
2014-03-01 18:07:47 +0200

    added serve and watch tasks to gulp and defined the default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
 var gulp = require('gulp'),
+    util = require('gulp-util'),
     path = require('path'),
     less = require('gulp-less');
 
-gulp.task('default', function() {
-  // default task
-});
+var http = require('http'),
+    ecstatic = require('ecstatic'),
+    serverPort = 8080;
 
-gulp.task('less', function() {
+var paths = {
+  app: path.join(__dirname, 'app'),
+  styles: path.join(__dirname, 'styles')
+}
+
+gulp.task('default', ['build', 'serve', 'watch']);
+
+gulp.task('build', ['styles']);
+
+gulp.task('styles', function() {
   gulp.src('./styles/main.less')
     .pipe(less({
-      paths: [ path.join(__dirname, 'styles') ]
+      paths: [ paths.styles ]
     }))
     .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.styles + '**/*.less', ['styles']);
+});
+
+gulp.task('serve', function() {
+  http.createServer(ecstatic( { root: paths.app })).listen(serverPort);
+  util.log('Started a server that listens to the port ' + serverPort + '...');
 });
