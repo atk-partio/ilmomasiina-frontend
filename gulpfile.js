0611b80bc95b0c2ec9a245e1f28bb5dc1023337f
Jaakko Hynynen <jaakko.hynynen@gmail.com>
2014-03-01 17:10:37 +0200

    gulp less compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    path = require('path'),
+    less = require('gulp-less');
 
 gulp.task('default', function() {
   // default task
+});
+
+gulp.task('less', function() {
+  gulp.src('./styles/main.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'styles') ]
+    }))
+    .pipe(gulp.dest('./app/css'));
 });
