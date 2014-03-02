567408b298d42eb1aa3aac2a08ff6998d365768a
Jaakko Hynynen <jaakko.hynynen@gmail.com>
2014-03-02 03:24:22 +0200

    fixed karma test failure not aborting build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     util = require('gulp-util'),
     path = require('path'),
+    spawn = require('child_process').spawn,
 
     less = require('gulp-less'),
     minifyCSS = require('gulp-minify-css'),
@@ -8,7 +9,8 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
 
-    karma = require('gulp-karma');
+    karmaRunner = require('karma').server,
+    karmaConfigFile = 'karma.conf.js';
 
 var http = require('http'),
     ecstatic = require('ecstatic'),
@@ -20,11 +22,31 @@ var paths = {
   scripts: path.join(__dirname, 'app', 'js')
 }
 
-gulp.task('default', ['run-tests', 'build', 'serve', 'watch']);
+function karmaSingleRun(callback, opts) {
+  opts || (opts = {});
+  opts.autoWatch = false;
+  opts.singleRun = true;
+  karmaRun(callback, opts);
+}
 
-gulp.task('build', ['run-tests', 'scripts', 'styles']);
+function karmaRun(callback, opts) {
+  var args, child;
+  opts.configFile = path.join(__dirname, karmaConfigFile);
 
-gulp.task('scripts', ['run-tests'], function() {
+  args = [ path.join(__dirname, 'test', 'utils', 'background_process.js'), JSON.stringify(opts)],
+  child = spawn('node', args, { stdio: 'inherit' });
+  child.on('exit', function(code) {
+    callback(code);
+  });
+}
+
+gulp.task('default', ['build-dev', 'serve', 'watch']);
+
+/* TASKS FOR DEVELOPMENT */
+
+gulp.task('build-dev', ['scripts-dev', 'karma-unit-singlerun', 'styles']);
+
+gulp.task('scripts-dev', function() {
   return gulp.src('./app/js/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter(stylish))
@@ -40,31 +62,34 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('./app/css'));
 });
 
-gulp.task('run-tests', function() {
-  var testFiles = [
-    'test/spec/**/*.js',
-    'app/bower_components/jquery/dist/jquery.js',
-    'app/bower_components/underscore/underscore.js',
-    'app/bower_components/angular/angular.js',
-    'app/bower_components/angular-route/angular-route.js',
-    'app/bower_components/angular-mocks/angular-mocks.js',
-    'app/js/*.js',
-    'app/js/**/*.js'
-  ];
-
-  return gulp.src(testFiles)
-    .pipe(karma({
-      configFile: './karma.conf.js',
-      action: 'run'
-    }));
-});
-
-gulp.task('watch', ['build'], function() {
-  gulp.watch([ './app/js/*.js', './app/js/**/*.js'], ['scripts']);
+gulp.task('watch', ['build-dev'], function() {
+  // when a .js file changes, process .js files and run unit tests
+  gulp.watch([ './app/js/*.js', './app/js/**/*.js'], ['scripts-dev', 'karma-unit-singlerun-lenient']);
+  // when a .less file changes, process .less files
   gulp.watch(paths.styles + '**/*.less', ['styles']);
 });
 
-gulp.task('serve', ['build'], function() {
+gulp.task('serve', ['build-dev'], function() {
   http.createServer(ecstatic( { root: paths.app })).listen(serverPort);
   util.log('Started a server that listens to the port ' + serverPort + '...');
+});
+
+// unit testing task that fails the build if tests do not pass
+gulp.task('karma-unit-singlerun', ['scripts-dev'], function(callback) {
+  karmaSingleRun(callback);
+});
+
+// unit testing task that never fails a build, used for running unit tests after .js file changes
+gulp.task('karma-unit-singlerun-lenient', ['scripts-dev'], function(callback) {
+  var cb = function() {
+    callback(0);
+  };
+  karmaSingleRun(cb);
+});
+
+/* TASKS FOR CONTINUOUS INTEGRATION */
+
+// unit testing task that fails the build, but runs the tests only on Firefox
+gulp.task('karma-unit-for-ci', ['scripts-dev'], function(callback) {
+  karmaSingleRun(callback, { browsers: ['Firefox'] });
 });
